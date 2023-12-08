79455a6e551c901aee4a0a0ab507915791dd80b0
h2s0 <h2s0_@naver.com>
2023-12-08 23:48:14 +0900

    ➕ 아이콘 이미지 파일 추가

--- src/assets/icons/svg-icons.js
+++ src/assets/icons/svg-icons.js
@@ -1,2 +1,20 @@
 // icons 폴더에 있는 파일을 한꺼번에 내보내는 파일입니다.
 // export { default as heartFill } from './heart-fill.svg'; 형식으로 작성합니다.
+
+export { default as alertCircle } from './alert-circle.svg';
+export { default as homeBlue } from './home-blue.svg';
+export { default as homeGrey } from './home-grey.svg';
+export { default as leftArrow } from './left-arrow.svg';
+export { default as meetingBlue } from './meeting-blue.svg';
+export { default as meetingGrey } from './meeting-grey.svg';
+export { default as menu } from './menu.svg';
+export { default as plus } from './plus.svg';
+export { default as profileBlue } from './profile-blue.svg';
+export { default as profileGrey } from './profile-grey.svg';
+export { default as quitCircle } from './quit-circle.svg';
+export { default as quit } from './quit.svg';
+export { default as searchBlack } from './search-black.svg';
+export { default as searchBlue } from './search-blue.svg';
+export { default as todoBlue } from './todo-blue.svg';
+export { default as todoGrey } from './todo-grey.svg';
+export { default as topArrow } from './top-arrow.svg';
