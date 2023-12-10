fcf57f1c793d6bb8ac8fbc11e5228dec1b0ed573
dongapple <hg971027@gmail.com>
2023-12-11 05:00:27 +0900

    🎨 상태별 css 적용 및 코드 변수화

--- src/components/ButtonRound.jsx
+++ src/components/ButtonRound.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ErrorMessage from './ErrorMessage';
 
 function ButtonRound({
   type = 'button',
   clicked = false,
-  seleted = false,
+  selected = false,
   error = false,
   isValid,
   errorMessage,
@@ -12,15 +12,40 @@ function ButtonRound({
   onClick,
   ...restProps
 }) {
-  const colorClass = clicked ? '' : seleted ? '' : error ? '' : '';
+  const colorClass = clicked
+    ? '-bg--primary-blue100 -text--system-black border-[1px] -border--primary-blue300'
+    : selected
+      ? '-bg--primary-blue500 -text--system-white'
+      : error
+        ? 'border-[1px] -border--system-danger -text--system-black -bg--system-white'
+        : 'border-[1px] -border--grey300 -text--system-black -bg--grey100';
 
-  return (
+  const hasValue = errorMessage !== '';
+
+  const renderButton = (
     <button
       type={type}
-      className={`px-4 py-1 text-body4 box-border rounded-lg  ${colorClass}`}
+      className={`px-4 py-1 text-body4 box-border rounded-3xl ${colorClass}`}
       {...restProps}
       onClick={onClick}
-    ></button>
+    />
+  );
+
+  const renderErrorMessage = hasValue && (
+    <ErrorMessage isValid={isValid} errorMessage={errorMessage} />
+  );
+
+  return (
+    <>
+      {hasValue ? (
+        <div className="flex flex-col items-start">
+          {renderButton}
+          {renderErrorMessage}
+        </div>
+      ) : (
+        renderButton
+      )}
+    </>
   );
 }
 
@@ -29,7 +54,7 @@ export default ButtonRound;
 ButtonRound.propTypes = {
   type: PropTypes.string,
   clicked: PropTypes.bool,
-  seleted: PropTypes.bool,
+  selected: PropTypes.bool,
   error: PropTypes.bool,
   onClick: PropTypes.func,
   isValid: PropTypes.bool,
