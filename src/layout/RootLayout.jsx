8301816d62bb7062fc209b21161301710f3a7189
h2s0 <h2s0_@naver.com>
2023-12-13 00:57:07 +0900

    🚑 페이지 컴포넌트들이 헤더 영역과 겹쳐 렌더링 되는 이슈 해결

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -6,29 +6,37 @@ import { useLocation } from 'react-router-dom';
 function RootLayout() {
   const { pathname } = useLocation();
 
-  // 탭바 존재하는 페이지에서 패딩 적용해주기
-  let isHiddenTabBarPadding = null;
+  // 탭바 존재하는 페이지에서 패딩 적용
+  let isHiddenTabBarPadding = 'pb-20';
+   // 헤더 존재하는 페이지에서 패딩 적용
+  let isHiddenHeaderPadding = 'pt-12';
 
   // TabBar 없는 페이지
-  if ( pathname === '/' ||
-        pathname === '/signin' ||
-        pathname === '/signup' ||
-        pathname === '/putcategory' ||
-        pathname === '/putduration' ||
-        pathname === '/putmembers' ||
-        pathname === '/putprojectname' ||
-        pathname === '/puttopic' ||
-        pathname === '/meetingminutesdetail' ||
-        pathname === '/meetingminutestext' ||
-        pathname === '/meetingminutesvoice'
+  if (
+      pathname === '/' ||
+      pathname === '/signin' ||
+      pathname === '/signup' ||
+      pathname === '/putcategory' ||
+      pathname === '/putduration' ||
+      pathname === '/putmembers' ||
+      pathname === '/putprojectname' ||
+      pathname === '/puttopic' ||
+      pathname === '/meetingminutesdetail' ||
+      pathname === '/meetingminutestext' ||
+      pathname === '/meetingminutesvoice'
     ) {
-      isHiddenTabBarPadding = null;
+      isHiddenTabBarPadding = '';
+    }
+
+  // Header 없는 페이지
+  if (pathname === '/' || pathname === '/signin') {
+    isHiddenHeaderPadding = '';
   }
 
   return (
     <>
       <Header />
-      <main className={`pt-12 ${!isHiddenTabBarPadding ? 'pb-20' : isHiddenTabBarPadding}`}>
+      <main className={`${isHiddenHeaderPadding} ${isHiddenTabBarPadding} px-4`}>
         <Outlet />
       </main>
       <TabBar />
