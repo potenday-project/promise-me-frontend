ba3775596f9c347be58c2d50ce9feca0d7dfee1d
dongapple <hg971027@gmail.com>
2023-12-12 17:05:19 +0900

    🎨 스타일링 완성

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { mic, noteTaking, editDefault } from '@/assets/icons/svg-icons';
 
 function FloatingButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,30 +19,34 @@ function FloatingButton() {
           onClick={closeOverlay}
           className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-60"
         >
-          <div className="absolute bottom-[calc(16px+60px)] right-4 flex flex-col gap-4 items-end  ">
-            <dl className="flex items-center">
+          <div className="absolute bottom-[calc(16px+60px)] right-4 flex flex-col gap-2 items-end text-title3 -text--system-white ">
+            <dl className="flex items-center gap-4 ">
               <dt>
                 <p>회의 녹음하기</p>
               </dt>
               <dd>
-                <button
-                  onClick={() => console.log('Button 1 clicked')}
-                  className="text-black bg-white rounded-full w-14 h-14 "
-                >
-                  Button 1
+                <button className="rounded-full">
+                  <img
+                    onClick={() => console.log('Button 1 clicked')}
+                    src={mic}
+                    alt=""
+                    className=""
+                  />
                 </button>
               </dd>
             </dl>
-            <dl className="flex items-center">
+            <dl className="flex items-center gap-4">
               <dt>
                 <p>회의록 직접 쓰기</p>
               </dt>
               <dd>
-                <button
-                  onClick={() => console.log('Button 2 clicked')}
-                  className="text-black bg-white rounded-full w-14 h-14"
-                >
-                  Button 2
+                <button className="rounded-full">
+                  <img
+                    onClick={() => console.log('Button 2 clicked')}
+                    src={noteTaking}
+                    alt=""
+                    className=""
+                  />
                 </button>
               </dd>
             </dl>
@@ -50,11 +55,9 @@ function FloatingButton() {
       )}
       <button
         onClick={handleClick}
-        className={`w-14 h-14 rounded-full focus:outline-none ${
-          isOpen ? 'bg-white text-black' : 'bg-blue-500 text-white'
-        }`}
+        className={` rounded-full  ${isOpen ? 'hidden' : ''}`}
       >
-        {isOpen ? '' : 'Open'}
+        <img src={editDefault} alt="" />
       </button>
     </div>
   );
