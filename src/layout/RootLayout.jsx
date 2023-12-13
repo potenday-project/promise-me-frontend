bdda7d370880f4cb039a91968e829bf1d74e5a00
dongapple <hg971027@gmail.com>
2023-12-13 18:54:20 +0900

    🌊 하단 패딩 수정, 배경색 추가

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -7,36 +7,45 @@ function RootLayout() {
   const { pathname } = useLocation();
 
   // 탭바 존재하는 페이지에서 패딩 적용
-  let isHiddenTabBarPadding = 'pb-20';
-   // 헤더 존재하는 페이지에서 패딩 적용
+  let isHiddenTabBarPadding = 'pb-[60px]';
+  // 헤더 존재하는 페이지에서 패딩 적용
   let isHiddenHeaderPadding = 'pt-12';
+  // 배경색 존재하는 페이지 배경색 적용
+  let isBackgrounColor = '-bg--primary-blue50';
 
   // TabBar 없는 페이지
   if (
-      pathname === '/' ||
-      pathname === '/signin' ||
-      pathname === '/signup' ||
-      pathname === '/putcategory' ||
-      pathname === '/putduration' ||
-      pathname === '/putmembers' ||
-      pathname === '/putprojectname' ||
-      pathname === '/puttopic' ||
-      pathname === '/meetingminutesdetail' ||
-      pathname === '/meetingminutestext' ||
-      pathname === '/meetingminutesvoice'
-    ) {
-      isHiddenTabBarPadding = '';
-    }
+    pathname === '/' ||
+    pathname === '/signin' ||
+    pathname === '/signup' ||
+    pathname === '/putcategory' ||
+    pathname === '/putduration' ||
+    pathname === '/putmembers' ||
+    pathname === '/putprojectname' ||
+    pathname === '/puttopic' ||
+    pathname === '/meetingminutesdetail' ||
+    pathname === '/meetingminutestext' ||
+    pathname === '/meetingminutesvoice'
+  ) {
+    isHiddenTabBarPadding = '';
+  }
 
   // Header 없는 페이지
   if (pathname === '/' || pathname === '/signin') {
     isHiddenHeaderPadding = '';
   }
 
+  // backgoundColor 있는 페이지
+  if (pathname === '/home' || pathname === '/TodoList') {
+    isBackgrounColor = ``;
+  }
+
   return (
     <>
       <Header />
-      <main className={`${isHiddenHeaderPadding} ${isHiddenTabBarPadding} px-4`}>
+      <main
+        className={`${isHiddenHeaderPadding} ${isHiddenTabBarPadding} ${isBackgrounColor} px-4 h-screen`}
+      >
         <Outlet />
       </main>
       <TabBar />
