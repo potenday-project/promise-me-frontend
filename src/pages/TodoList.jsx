ebea77fad2f7bcb73717f61ba80bc257dda006f1
dongapple <hg971027@gmail.com>
2023-12-13 19:48:12 +0900

    ✨ 기본 마크업 구성

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,34 @@
-function TodoList() {
+import TodoItem from '@/components/TodoItem';
+import { triangleLeft, triangleRight } from '@/assets/icons/svg-icons';
 
+function TodoList() {
+  return (
+    <section className="flex flex-row items-center w-full h-screen -bg--primary-blue50">
+      <button>
+        <img src={triangleLeft} alt="" />
+      </button>
+      <div className="w-[248px] -bg--system-white rounded-3xl mx-auto h-auto p-4 flex flex-col border-[1px] -border--grey300">
+        <button className="mb-6">날짜 버튼</button>
+        <ul className="flex flex-col gap-2">
+          <li>
+            <TodoItem text={`컬러칩체크하기`}></TodoItem>
+          </li>
+          <li>
+            <TodoItem text={`컬러칩체크하기`}></TodoItem>
+          </li>
+          <li>
+            <TodoItem text={`컬러칩체크하기`}></TodoItem>
+          </li>
+          <li>
+            <input type="text" />
+          </li>
+        </ul>
+      </div>
+      <button>
+        <img src={triangleRight} alt="" />
+      </button>
+    </section>
+  );
 }
 
 export default TodoList;
