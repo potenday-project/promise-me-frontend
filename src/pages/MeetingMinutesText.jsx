1575551adff67a9294d7ec2b85776a012565d731
dongapple <hg971027@gmail.com>
2023-12-15 00:13:28 +0900

    ✨ 서버와의 통신

--- src/pages/MeetingMinutesText.jsx
+++ src/pages/MeetingMinutesText.jsx
@@ -1,10 +1,11 @@
 import ButtonBox from '@/components/ButtonBox';
 import TitleTextBox from '@/components/TitleTextBox';
 import { useState, useEffect } from 'react';
-import {} from 'react';
+import axios from 'axios';
 
 function MeetingMinutesText() {
   const [text, setText] = useState('');
+  const projectId = 1; // 프로젝트 식별 번호를 적절한 값으로 설정해주세요.
 
   useEffect(() => {
     const textareaElement = document.getElementById('dynamic-textarea');
@@ -12,10 +13,29 @@ function MeetingMinutesText() {
     textareaElement.style.height = `${textareaElement.scrollHeight + 2}px`;
   }, [text]);
 
+  // 서버로 데이터를 보내는 함수
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost:8080/meeting/summary',
+        {
+          meeting_content: text,
+          project_id: projectId,
+        }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <section>
       <TitleTextBox title="회의록을 기록해주세요" />
-      <form>
+      <form onSubmit={handleSubmit}>
         <textarea
           id="dynamic-textarea"
           className="w-full min-h-[138px] p-4 text-body4 -bg--primary-blue50 content-start focus:outline-none border-[1px] rounded-lg"
@@ -23,7 +43,7 @@ function MeetingMinutesText() {
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <div>
+        <div className="fixed w-[calc(100vw-32px)] bottom-4">
           <ButtonBox type="submit">저장하기</ButtonBox>
         </div>
       </form>
