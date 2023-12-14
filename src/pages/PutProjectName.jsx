f08bbfa4b41defeadfc827c0381afb06c831a122
h2s0 <h2s0_@naver.com>
2023-12-14 15:14:57 +0900

    🎨 PutPorjectName.jsx : ui 완료

--- src/pages/PutProjectName.jsx
+++ src/pages/PutProjectName.jsx
@@ -1,5 +1,27 @@
-function PutProjectName() {
+import ButtonBox from "@/components/ButtonBox";
+import PlaceholderLine from "@/components/PlaceHolderLine";
 
+function PutProjectName() {
+  return (
+    <>
+      <p className="mt-6 mb-7 text-headline2">
+        프로젝트 관리를 시작할게요 <br/>
+        팀 이름을 알려주세요
+      </p>
+      <PlaceholderLine
+        name="teamname"
+        label="팀 이름"
+        placeholder="팀 이름을 입력해주세요"
+        type="text"
+      />
+      <ButtonBox
+        type="button"
+        navigateTo="/putcategory">
+          확인
+      </ButtonBox>
+      
+    </>
+  )
 }
 
 export default PutProjectName;
