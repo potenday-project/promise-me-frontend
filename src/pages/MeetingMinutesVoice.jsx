bcf23d586593eea1c3789314212f5fc7aa1265bb
dongapple <hg971027@gmail.com>
2023-12-15 01:23:07 +0900

    ✨ 음성 받아쓰기 성공

--- src/pages/MeetingMinutesVoice.jsx
+++ src/pages/MeetingMinutesVoice.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 import ButtonBox from '@/components/ButtonBox';
 
 import TitleTextBox from '@/components/TitleTextBox';
-// import { useEffect } from 'react';
 
 const MeetingMinutesVoice = () => {
   const [stream, setStream] = useState();
@@ -12,6 +11,82 @@ const MeetingMinutesVoice = () => {
   const [source, setSource] = useState();
   const [analyser, setAnalyser] = useState();
   const [audioUrl, setAudioUrl] = useState();
+
+  const convertToWav = async (blob) => {
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+    const reader = new FileReader();
+
+    return new Promise((resolve) => {
+      reader.onloadend = () => {
+        audioContext.decodeAudioData(reader.result, (buffer) => {
+          const offlineContext = new OfflineAudioContext({
+            numberOfChannels: buffer.numberOfChannels,
+            length: buffer.length,
+            sampleRate: buffer.sampleRate,
+          });
+
+          const source = offlineContext.createBufferSource();
+          source.buffer = buffer;
+
+          const destination = offlineContext.destination;
+          source.connect(destination);
+
+          source.start();
+          offlineContext.startRendering().then((renderedBuffer) => {
+            const wavBlob = new Blob([encodeWav(renderedBuffer)], {
+              type: 'audio/wav',
+            });
+            resolve(wavBlob);
+          });
+        });
+      };
+
+      reader.readAsArrayBuffer(blob);
+    });
+  };
+
+  const encodeWav = (audioBuffer) => {
+    const numOfChan = audioBuffer.numberOfChannels;
+    const length = audioBuffer.length * numOfChan * 2 + 44;
+    const buffer = new ArrayBuffer(length);
+    const view = new DataView(buffer);
+
+    const writeString = (view, offset, str) => {
+      for (let i = 0; i < str.length; i++) {
+        view.setUint8(offset + i, str.charCodeAt(i));
+      }
+    };
+
+    const floatTo16BitPCM = (output, offset, input) => {
+      for (let i = 0; i < input.length; i++, offset += 2) {
+        const s = Math.max(-1, Math.min(1, input[i]));
+        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+      }
+    };
+
+    writeString(view, 0, 'RIFF');
+    view.setUint32(4, 44 + length, true);
+    writeString(view, 8, 'WAVE');
+    writeString(view, 12, 'fmt ');
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+    view.setUint16(22, numOfChan, true);
+    view.setUint32(24, audioBuffer.sampleRate, true);
+    view.setUint32(28, audioBuffer.sampleRate * 2 * numOfChan, true);
+    view.setUint16(32, numOfChan * 2, true);
+    view.setUint16(34, 16, true);
+    writeString(view, 36, 'data');
+    view.setUint32(40, length - 44, true);
+
+    floatTo16BitPCM(view, 44, audioBuffer.getChannelData(0));
+
+    if (numOfChan > 1) {
+      floatTo16BitPCM(view, 44 + length, audioBuffer.getChannelData(1));
+    }
+
+    return view;
+  };
 
   const onRecAudio = () => {
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
@@ -41,8 +116,9 @@ const MeetingMinutesVoice = () => {
           analyser.disconnect();
           audioCtx.createMediaStreamSource(stream).disconnect();
 
-          mediaRecorder.ondataavailable = function (e) {
-            setAudioUrl(e.data);
+          mediaRecorder.ondataavailable = async function (e) {
+            const wavBlob = await convertToWav(e.data);
+            setAudioUrl(wavBlob);
             setOnRec(true);
           };
         } else {
@@ -53,8 +129,9 @@ const MeetingMinutesVoice = () => {
   };
 
   const offRecAudio = () => {
-    media.ondataavailable = function (e) {
-      setAudioUrl(e.data);
+    media.ondataavailable = async function (e) {
+      const wavBlob = await convertToWav(e.data);
+      setAudioUrl(wavBlob);
       setOnRec(true);
     };
 
@@ -68,14 +145,14 @@ const MeetingMinutesVoice = () => {
   };
 
   const onSubmitAudioFile = useCallback(() => {
-    const sound = new File([audioUrl], 'soundBlob.mp3', {
+    const sound = new File([audioUrl], 'soundBlob.wav', {
       lastModified: new Date().getTime(),
-      type: 'audio/mpeg',
+      type: 'audio/wav',
     });
     console.log('파일:', sound);
 
     const formData = new FormData();
-    formData.append('voiceFile', sound, 'soundBlob.mp3');
+    formData.append('voiceFile', sound, 'soundBlob.wav');
 
     axios
       .post('http://localhost:8080/meeting/transfer', formData)
@@ -87,26 +164,19 @@ const MeetingMinutesVoice = () => {
       });
   }, [audioUrl]);
 
-  // useEffect(() => {
-  //   onRecAudio();
-  //   return () => {
-  //     offRecAudio();
-  //   };
-  // }, []);
-
   return (
-    <>
-      <div className="flex flex-col gap-2 ">
-        <button
-          className="px-10 bg-red-400"
-          onClick={onRec ? onRecAudio : offRecAudio}
-        >
-          녹음
-        </button>
-        <button className="bg-blue-300" onClick={onSubmitAudioFile}>
-          결과 확인
-        </button>
-      </div>
+    <div className="flex flex-col gap-2 ">
+      <button
+        className="px-10 bg-red-400"
+        onClick={onRec ? onRecAudio : offRecAudio}
+      >
+        녹음
+      </button>
+      <button className="bg-blue-300" onClick={onSubmitAudioFile}>
+        결과 확인
+      </button>
+
+      <div className="flex flex-col gap-2 "></div>
       <section className="relative flex flex-col justify-between h-full ">
         <TitleTextBox title="녹음을 시작합니다."></TitleTextBox>
         <div className="flex flex-col items-center gap-4 m-9">
@@ -116,15 +186,22 @@ const MeetingMinutesVoice = () => {
             className="-bg--grey100 w-[240px] h-[240px]"
           ></img>
           <p className="text-center text-title4">
-            녹음을 완료하시면 녹음한 내용을
-            <br /> AI가 요약해드려요
+            {onRec ? (
+              '캐릭터를 눌러 녹음을 시작해 주세요'
+            ) : (
+              <>
+                녹음을 완료하시면 녹음한 내용을 <br /> AI가 요약해드려요
+              </>
+            )}
           </p>
         </div>
         <div className="fixed w-[calc(100vw-32px)] bottom-4">
-          <ButtonBox>녹음완료</ButtonBox>
+          <ButtonBox onClick={offRecAudio} disable={onRec}>
+            녹음완료
+          </ButtonBox>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
