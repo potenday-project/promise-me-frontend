ced67d50fae60c9de28f6fcd4718df7fd8625591
dongapple <hg971027@gmail.com>
2023-12-14 21:52:58 +0900

    🎨 마크업 추가

--- src/pages/MeetingMinutesVoice.jsx
+++ src/pages/MeetingMinutesVoice.jsx
@@ -1,5 +1,9 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import axios from 'axios';
+import ButtonBox from '@/components/ButtonBox';
+
+import TitleTextBox from '@/components/TitleTextBox';
+// import { useEffect } from 'react';
 
 const MeetingMinutesVoice = () => {
   const [stream, setStream] = useState();
@@ -83,18 +87,44 @@ const MeetingMinutesVoice = () => {
       });
   }, [audioUrl]);
 
+  // useEffect(() => {
+  //   onRecAudio();
+  //   return () => {
+  //     offRecAudio();
+  //   };
+  // }, []);
+
   return (
-    <div className="flex flex-col gap-2 ">
-      <button
-        className="px-10 bg-red-400"
-        onClick={onRec ? onRecAudio : offRecAudio}
-      >
-        녹음
-      </button>
-      <button className="bg-blue-300" onClick={onSubmitAudioFile}>
-        결과 확인
-      </button>
-    </div>
+    <>
+      <div className="flex flex-col gap-2 ">
+        <button
+          className="px-10 bg-red-400"
+          onClick={onRec ? onRecAudio : offRecAudio}
+        >
+          녹음
+        </button>
+        <button className="bg-blue-300" onClick={onSubmitAudioFile}>
+          결과 확인
+        </button>
+      </div>
+      <section className="relative flex flex-col justify-between h-full ">
+        <TitleTextBox title="녹음을 시작합니다."></TitleTextBox>
+        <div className="flex flex-col items-center gap-4 m-9">
+          <img
+            src=""
+            alt="녹음중입니다."
+            className="-bg--grey100 w-[240px] h-[240px]"
+          ></img>
+          <p className="text-center text-title4">
+            녹음을 완료하시면 녹음한 내용을
+            <br /> AI가 요약해드려요
+          </p>
+        </div>
+        <div className="fixed w-[calc(100vw-32px)] bottom-4">
+          <ButtonBox>녹음완료</ButtonBox>
+        </div>
+      </section>
+    </>
   );
 };
 
