cea7bcf2add353bce7d23d953091407e9d8644b3
h2s0 <h2s0_@naver.com>
2023-12-16 00:31:48 +0900

    ✨ 이메일 존재 유효성 검사, zustand 에 정보 저장, 프로젝트 생성 위한 정보 서버로 전달, 이메일 삭제 기능 구현

--- src/pages/PutMembers.jsx
+++ src/pages/PutMembers.jsx
@@ -2,11 +2,15 @@ import ButtonBox from "@/components/ButtonBox";
 import ButtonRound from "@/components/ButtonRound";
 import PlaceholderLine from "@/components/PlaceHolderLine";
 import PlaceholderRound from "@/components/PlaceholderRound";
-import TextBox from "@/components/TextBox";
+import useProjectStore from "@/store/project";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 function PutMembers() {
+
+  const navigate = useNavigate();
+
   // 작업을 위한 예시 데이터, 나중에 지우기
   const exampleData = [
     '프로젝트 매니저',
@@ -18,15 +22,7 @@ function PutMembers() {
   const [email, setEmail] = useState('');
   const [counts, setCounts] = useState({});
   const [isValid, setIsValid] = useState(null);
-
-  useEffect(() => {
-    // 모든 직군 카운트 초기값 0
-    const initialCounts = roles.reduce((obj, role) => ({
-      ...obj,
-      [role]: 0,
-    }), {});
-    setCounts(initialCounts);
-  }, [roles]);
+  const [isDisabled, setIsDisabled] = useState(true);
 
   // useEffect(() => {
   //   axios.get('') // api
@@ -43,6 +39,7 @@ function PutMembers() {
     return regex.test(email);
   }
 
+  // 이메일 입력 값을 상태로 관리, 유효성 검사
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
     const valid = isValidEmail(event.target.value)
@@ -50,10 +47,67 @@ function PutMembers() {
     console.log(valid)
   };
 
+  // 각 역할 버튼 클릭시, 서버 이메일 존재 여부 요청
+  // 유효하다면 해당 역할에 이메일 추가, zustand 정보 저장
+  //존재하지 않는 사용자인 경우 메세지 표시
   const handleButtonClick = (role) => {
-    setCounts({
-      ...counts,
-      [role]: counts[role] + 1,
+    if(isValid) {
+      axios
+      .post('', { email })// 이메일 검증 api/users/check, {email} 도 바꿔야할 수도 있음
+      .them(response => {
+        const { data } = response;
+        if(data.user_id) {
+          setCounts({
+            ...counts,
+            [role]: counts[role] ? [...counts[role], email] : [email], // 직군별로 이메일 그룹화
+          });
+          // zustand 스토어에 저장
+          useProjectStore.getState().setMembers(counts);
+          setIsDisabled(false) // 확인 버튼 활성화
+        } else {
+          alert(data.message);
+        }
+        setEmail('') //이메일 초기화
+      })
+      .catch(error => {
+        console.error('Error verifying email:',error) //오류처리
+      })
+    }
+  };
+
+  // 확인 버튼 누르면 zustand 의 정보를 꺼내 서버로 전달
+  const handleConfirmClick = () => {
+    const { name, category, start, deadline, members } = useProjectStore.getState();
+    // http://localhost:8080/project/create
+    axios.post('', {
+      name,
+      category,
+      memberList: members,
+      start,
+      deadline,
+    })
+    .then(response => {
+      if(response.state === 200 ) {
+        alert('프로젝트 생성에 성공하였습니다.');
+        navigate('/projectInfo');
+      } else {
+        alert('프로젝트 생성에 실패하였습니다.')
+      }
+    })
+    .catch(error => {
+      console.error('Error creating project:', error); // 오류 처리
+    });
+  };
+
+  // 이메일 삭제
+  const handleDeleteEmail = (role, email) => {
+    setCounts((prevCounts) => {
+      const emails = prevCounts[role];
+      if (!emails) {
+        return prevCounts; // prevCounts[role]이 undefined인 경우 prevCounts를 그대로 반환
+      }
+      const updatedEmails = emails.filter((e) => e !== email);
+      return { ...prevCounts, [role]: updatedEmails };
     });
   };
 
@@ -76,7 +130,7 @@ function PutMembers() {
         {roles && roles.map((role, index) => ( // roles 가 정의되었는지 확인, 추후 수정 가능
           <li key={index}>
             <ButtonRound
-              status="" // 이메일이 입력 완료 되면 clicked 로 변경
+              status={counts[role] && counts[role].length > 0 ? 'clicked' : ''} // 이메일이 추가되었으면 파랑색으로 변경
               onClick={() => handleButtonClick(role)} // 클릭하면 카운트, 아래에 이메일 입력
             >
               {role}
@@ -84,19 +138,29 @@ function PutMembers() {
           </li>
         ))}
       </ul>
-      <div className="bg-blue-50 border -border--grey300 rounded-lg p-4">
-        {roles && roles.map((role, index) => (
+      <div className="bg-blue-50 border -border--grey300 rounded-lg p-4 flex flex-col gap-6">
+        {Object.entries(counts).map(([role, emails], index) => (
           <div key={index}>
-          <p className="text-title4">{role} {counts[role]}명</p>
+            <p className="text-title4">
+              {role} {emails.length}명
+            </p>
+            {emails.map((email, index) => (
+              <PlaceholderLine
+                key={index}
+                values={email}
+                isValid={true}
+                readonly={true}
+                clickDelete={true}
+                deletInput={() => handleDeleteEmail(role, email)}
+              />
+            ))}
           </div>
         ))}
-        <PlaceholderLine></PlaceholderLine>
       </div>
       <div className="fixed w-[calc(100vw-32px)] bottom-4">
         <ButtonBox
-          navigateTo = '/projectInfo'
-          // 이메일 형식 유효성 검사 통과 시 활성화되어 있지만, 나중에 팀원을 최소 1명 선택해야 활성화되게 수정하기
-          disable={!isValid}
+          disable={isDisabled}
+          onClick={handleConfirmClick}
         >
           확인
         </ButtonBox>
