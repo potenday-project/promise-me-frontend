4e85c396e01da632726ef61f32cdd8780fc5799d
dongapple <hg971027@gmail.com>
2023-12-15 18:22:48 +0900

    ♻️ 날짜 표현 수정 및 코드 정리

--- src/pages/MeetingMinutesDetail.jsx
+++ src/pages/MeetingMinutesDetail.jsx
@@ -1,51 +1,56 @@
-import ButtonMeetingMinutes from '@/components/ButtonMeetingMinutes';
-import TextBox from '@/components/TextBox';
-import TitleTextBox from '@/components/TitleTextBox';
-import axios from 'axios';
+// 필요한 모듈 불러오기
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import TextBox from '@/components/TextBox';
+import TitleTextBox from '@/components/TitleTextBox';
 
 function MeetingMinutesDetail() {
-  const { meetingId } = useParams(); // URL에서 ID 추출
-  const [detailData, setDetailData] = useState({}); // 상세 데이터를 저장할 state
   const [meetingMinutes, setMeetingMinutes] = useState([]);
+  const [formattedDate, setFormattedDate] = useState('');
+
+  const { meetingId } = useParams();
+
+  const meeting = meetingMinutes.find((data) => data.meetingId == meetingId);
 
   useEffect(() => {
-    console.log('버튼 아이디', meetingId);
+    if (meeting && meeting.meetingDate) {
+      const date = new Date(meeting.meetingDate);
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+      };
+      const formatter = new Intl.DateTimeFormat('ko-KR', options);
+      setFormattedDate(formatter.format(date));
+    }
+  }, [meeting]);
+
+  useEffect(() => {
     axios
       .get('http://localhost:8080/meeting/', {
-        params: {
-          projectId: 1,
-        },
+        params: { projectId: 1 },
       })
       .then((response) => {
         setMeetingMinutes(response.data);
-        console.log('입력 데이터', meetingMinutes);
       })
       .catch((error) => {
         console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
       });
-  }, [meetingId]); // ID가 변경될 때마다 데이터를 다시 가져옴
+  }, [meetingId]);
 
   return (
-    <section className="flex flex-col gap-6">
-      {meetingMinutes.map((data, index) =>
-        data.meetingId == meetingId ? (
-          <>
-            <TitleTextBox
-              key={index}
-              title={data.meetingDate || '날짜나 이름'}
-              sub={`홍길동님을 위해 AI가 회의를 요약했어요`}
-            />
-            <TextBox summary={'회의 요약'} content={data.summary}></TextBox>
-            <TextBox
-              summary={'회의 내용'}
-              content={data.meetingContent}
-            ></TextBox>
-          </>
-        ) : (
-          <></>
-        )
+    <section className="flex flex-col gap-6 pb-4">
+      {meeting && (
+        <>
+          <TitleTextBox
+            title={formattedDate}
+            sub={`홍길동님을 위해 AI가 회의를 요약했어요`}
+          />
+          <TextBox summary={'회의 요약'} content={meeting.summary} />
+          <TextBox summary={'회의 내용'} content={meeting.meetingContent} />
+        </>
       )}
     </section>
   );
