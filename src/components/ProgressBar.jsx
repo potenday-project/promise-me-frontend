dce9560b0385bf94bbf348a3940c704780717566
dongapple <hg971027@gmail.com>
2023-12-17 07:12:14 +0900

    ✨ 홈 부분

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,38 +1,40 @@
 import { barChart, clock } from '@/assets/icons/svg-icons.js';
 import { useState, useEffect } from 'react';
 
-function ProgressBar ({ percent, dday }) {
+function ProgressBar({ percent, dday }) {
   // prop 상태 관리, 기본값 null 로 설정
   const [loadingPercent, setLoadingPercent] = useState(null);
   const [loadingDday, setLoadingDday] = useState(null);
 
-  useEffect( () => {
+  useEffect(() => {
     setLoadingPercent(percent == null);
   }, [percent]);
 
-  useEffect( () => {
+  useEffect(() => {
     setLoadingDday(dday == null);
   }, [dday]);
 
   return (
-    <div className='flex gap-2'>
-      <div className='percent flex items-center gap-1'>
-        <img src={barChart}/>
-        <p className='text-headline4 mr-1'>진척도</p>
-        <p className='text-headline4 -text--primary-blue700'>
+    <div className="flex gap-2">
+      <div className="flex items-center gap-1 percent">
+        <img src={barChart} />
+        <p className="mr-1 text-headline4">진척도</p>
+        <p className="text-headline4 -text--primary-blue700">
           {loadingPercent ? '계산 중...' : `${percent}%`}
         </p>
       </div>
-      <span className='dday' aria-hidden='true'>|</span>
-      <div className='flex items-center gap-1'>
-        <img src={clock}/>
-        <p className='text-headline4 mr-1'>남은 기간</p>
-        <p className='text-headline4 -text--primary-blue700'>
-          {loadingDday ? '계산 중...' : <time dateTime=''>{dday}일</time>}
+      <span className="dday" aria-hidden="true">
+        |
+      </span>
+      <div className="flex items-center gap-1">
+        <img src={clock} />
+        <p className="mr-1 text-headline4">남은 기간</p>
+        <p className="text-headline4 -text--primary-blue700">
+          {loadingDday ? '계산 중...' : <time dateTime="">{dday}일</time>}
         </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default ProgressBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -135,11 +135,20 @@ function Home() {
           {!isLoading &&
             data.todoAll
               .filter((allData) => allData.role === isSelectedButton)
-              .map((filteredData, index) => {
+              .reduce((unique, item) => {
+                return unique.some(
+                  (uniqueItem) =>
+                    uniqueItem.recommendation === item.recommendation
+                )
+                  ? unique
+                  : [...unique, item];
+              }, [])
+              .map((uniqueData, index) => {
+                // 중복이 제거된 데이터를 출력합니다.
+                // 출력 형식은 필요에 따라 수정해 주세요.
                 return (
                   <div key={index}>
-                    <h3>{filteredData.recommendation}</h3>{' '}
-                    {/* 예: 이름을 출력 */}
+                    <h3>{uniqueData.recommendation}</h3>
                   </div>
                 );
               })}
