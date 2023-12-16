1bc3cad02abf0fc67dcd9b7831403b4bea434813
dongapple <hg971027@gmail.com>
2023-12-17 04:10:13 +0900

    ✨ 데이터 세팅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,95 +12,82 @@ import MyCalendar from '@/components/my-calendar/MyCalendar';
 import ButtonRound from '@/components/ButtonRound';
 import TodoItem from '@/components/TodoItem';
 import { useEffect } from 'react';
+import { useContext } from 'react';
+import { UserContext } from '@/contexts/UserContext';
+import axios from 'axios';
 
 function Home() {
-
+  const { userId, projectId, setProjectId } = useContext(UserContext);
   // 나중에 불러온 데이터로 바꿔주기
   let members = ['기획자', '디자이너', '프론트엔드 개발자', '백엔드 개발자'];
-  const [todoArray, setTodoArray] = useState([
-    {
-      name: '김희소',
-      role: '프론트엔드 개발자',
-      todo: [
-        { text: '컬러칩 체크하기', isChecked: false },
-        {
-          text: '컬러칩 체크하기',
-          isChecked: false,
-        },
-      ],
-    },
-    {
-      name: '신동혁',
-      role: '프론트엔드 개발자',
-      todo: [
-        { text: '컬러칩 체크하기', isChecked: false },
-        {
-          text: '컬러칩 체크하기',
-          isChecked: false,
-        },
-        {
-          text: '컬러칩 체크하기',
-          isChecked: true,
-        },
-      ],
-    },
-    {
-      name: '황초원',
-      role: '기획자',
-      todo: [
-        { text: '두부 발톱 잘라주기', isChecked: false },
-        {
-          text: '두부 산책 시키기',
-          isChecked: true,
-        },
-      ],
-    },
-  ]);
+  const [isProjectData, setIsProjectData] = useState([]);
+  const [isTodoArray, setIsTodoArray] = useState([]);
+  const [isProgress, setIsProgress] = useState(null);
+  const [isDday, setIsDday] = useState(null);
+  const [isSelectedButton, setIsSelectedButton] = useState(null);
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        // 데이터를 가져오는 비동기 요청
-        const response = await fetch(''); // api 넣기 http://43.201.85.197/
-        const data = await response.json();
+        const [projectDataRes, dDayRes, progressRes] = await Promise.all([
+          axios.get('http://43.201.85.197/project/', {
+            params: {
+              userId: userId,
+            },
+          }),
+          axios.get('http://43.201.85.197/project/dday', {
+            params: {
+              userId: userId,
+            },
+          }),
+          axios.get('http://43.201.85.197/project/progress', {
+            params: {
+              userId: userId,
+            },
+          }),
+        ]);
 
-        setTodoArray(data.rodoArray);
+        setIsProjectData(projectDataRes.data);
+        setIsDday(dDayRes.data);
+        setIsProgress(progressRes.data);
+        console.log(projectDataRes, dDayRes, progressRes);
       } catch (error) {
-        console.error('데이터를 불러오는 중 오류가 발생하였습니다.', error)
+        console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
       }
     };
+
     fetchData();
-  }, []);
+  }, [projectId]);
 
   // 버튼 클릭 상태 관리
-  const [selectedButton, setSelectedButton] = useState(null);
+
   // const [selectedDate, setSelectedDate] = useState(null);
 
   // const handleDateSelect = (date) => {
-    //setSelectedDate(date);
-    // api 요청을 통해 해당 날짜의 todoItem 을 가져져오거나, 상태를 업데이트하는 등의 작업
+  //setSelectedDate(date);
+  // api 요청을 통해 해당 날짜의 todoItem 을 가져져오거나, 상태를 업데이트하는 등의 작업
   //}
   return (
     <>
-    <section className='bg-blue-50 py-10'>
-      <Swiper
-        modules={[Navigation]}
-        navigation={{
-          clickable: true,
-          prevEl: '.swiper-button-prev',
-          nextEl: '.swiper-button-next',
-        }}
-      >
-        <SwiperSlide>
-          <img src={chevronRight} className="swiper-button-next" />
-          <ProgressAll />
-        </SwiperSlide>
-        <SwiperSlide>
-          <MyCalendar/>
-        </SwiperSlide>
-      </Swiper>
-    </section>
-      <section className="border-t -border--grey300 bg-white pl-4 pb-2">
+      <section className="py-10 bg-blue-50">
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            clickable: true,
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+          }}
+        >
+          <SwiperSlide>
+            <img src={chevronRight} className="swiper-button-next" />
+            <ProgressAll />
+          </SwiperSlide>
+          <SwiperSlide>
+            <MyCalendar />
+          </SwiperSlide>
+        </Swiper>
+      </section>
+      <section className="pb-2 pl-4 bg-white border-t -border--grey300">
         <Swiper
           slidesPerView="auto"
           freeMode={true}
@@ -111,10 +98,10 @@ function Home() {
           {members.map((member, index) => (
             <SwiperSlide style={{ width: 'auto' }} key={index}>
               <ButtonRound
-                status={selectedButton === member ? 'selected' : 'default'}
+                status={isSelectedButton === member ? 'selected' : 'default'}
                 onClick={() => {
-                  setSelectedButton(member);
-                  console.log(selectedButton);
+                  setIsSelectedButton(member);
+                  console.log(isSelectedButton);
                 }}
               >
                 {member}
@@ -122,12 +109,14 @@ function Home() {
             </SwiperSlide>
           ))}
         </Swiper>
-        <p className="text-headline4 mb-4 pr-4">AI가 추천해준 큰 일정...이것도 서버에서 받아와야함</p>
+        <p className="pr-4 mb-4 text-headline4">
+          AI가 추천해준 큰 일정...이것도 서버에서 받아와야함
+        </p>
         <ul className="flex flex-col gap-6">
-          {todoArray.map((person, index) => {
-            if (selectedButton === person.role) {
+          {isTodoArray.map((person, index) => {
+            if (isSelectedButton === person.role) {
               return (
-                <li key={index} className='flex flex-col gap-2'>
+                <li key={index} className="flex flex-col gap-2">
                   <p>{person.name}</p>
                   {person.todo.map((todo, todoIndex) => (
                     <TodoItem
