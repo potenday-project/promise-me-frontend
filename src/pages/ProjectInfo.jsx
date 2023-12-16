d5e6a4fe52f8c5a7ff1c07ec6d593ca70f94f6c7
h2s0 <h2s0_@naver.com>
2023-12-17 06:06:43 +0900

    🔥 불필요한 코드 삭제

--- src/pages/ProjectInfo.jsx
+++ src/pages/ProjectInfo.jsx
@@ -1,111 +1,24 @@
 import ButtonBox from "@/components/ButtonBox";
 import TextBox from "@/components/TextBox";
 import useProjectStore from "@/store/project";
-import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
 
 function ProjectInfo() {
-  // 서버에서 받아온 데이터를 저장할 상태
-  const teamname = useProjectStore(state => state.teamname);
-  const [schedule, setSchedule] = useState([
-    [
-      {
-          "role": "기획자",
-          "task": "사용자 요구사항 정의서 작성",
-          "start": "2023-12-08",
-          "finish": "2023-12-10"
-      },
-      {
-          "role": "기획자",
-          "task": "기능 명세서 작성",
-          "start": "2023-12-11",
-          "finish": "2023-12-15"
-      },
-      {
-          "role": "기획자",
-          "task": "화면 설계서 작성",
-          "start": "2023-12-16",
-          "finish": "2023-12-20"
-      },
-      {
-          "role": "프론트엔드 개발자",
-          "task": "HTML/CSS 레이아웃 구성",
-          "start": "2023-12-21",
-          "finish": "2023-12-25"
-      },
-      {
-          "role": "프론트엔드 개발자",
-          "task": "JavaScript 기능 구현",
-          "start": "2023-12-26",
-          "finish": "2023-12-30"
-      },
-      {
-          "role": "프론트엔드 개발자",
-          "task": "반응형 웹 적용",
-          "start": "2023-12-31",
-          "finish": "2023-12-31"
-      },
-      {
-          "role": "백엔드 개발자",
-          "task": "DB 스키마 작성",
-          "start": "2023-12-08",
-          "finish": "2023-12-10"
-      },
-      {
-          "role": "백엔드 개발자",
-          "task": "API 기능 구현",
-          "start": "2023-12-11",
-          "finish": "2023-12-20"
-      },
-      {
-          "role": "백엔드 개발자",
-          "task": "서버 구축 및 배포",
-          "start": "2023-12-21",
-          "finish": "2023-12-25"
-      },
-      {
-          "role": "디자이너",
-          "task": "로고 디자인",
-          "start": "2023-12-08",
-          "finish": "2023-12-10"
-      },
-      {
-          "role": "디자이너",
-          "task": "UI/UX 디자인",
-          "start": "2023-12-11",
-          "finish": "2023-12-20"
-      }
-  ]
-  ]);
-
-  // useEffect(() => {
-  //   axios.get('http://43.201.85.197/recommend/schedule') // API
-  //     .then(response => {
-  //       // 데이터를 상태로 설정, 수정필요
-  //       setSchedule(response.data);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching data: ', error);
-  //     });
-  // }, []);
-
-
-const groupedSchedule = schedule[0].reduce((groups, item) => {
-  const group = groups.find(group => group.role === item.role);
-  if (group) {
-    group.tasks.push(item);
-  } else {
-    groups.push({ role: item.role, tasks: [item] });
-  }
-  return groups;
-}, []);
-
-
+  const schedule = useProjectStore(state => state.projectSchedule);
+  
+  const groupedSchedule = schedule.length > 0 ? schedule[0].reduce((groups, item) => {
+    const group = groups.find(group => group.role === item.role);
+    if (group) {
+      group.tasks.push(item);
+    } else {
+      groups.push({ role: item.role, tasks: [item] });
+    }
+    return groups;
+  }, []) : [];
+  
   return(
     <>
       <p className="mt-6 mb-7 text-headline2">
-          {teamname}님을 위해<br/>
+          새싹님을 위해<br/>
           AI가 프로젝트 일정을 짜왔어요
       </p>
       <div className="flex flex-col gap-3 mb-5">
