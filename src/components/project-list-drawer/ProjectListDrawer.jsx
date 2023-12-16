ebc5474235e93c8972685634561a85524ad96fe3
dongapple <hg971027@gmail.com>
2023-12-17 03:39:17 +0900

    ✨ 데이터 불러오고 버튼 활성화 등

--- src/components/project-list-drawer/ProjectListDrawer.jsx
+++ src/components/project-list-drawer/ProjectListDrawer.jsx
@@ -1,29 +1,32 @@
-import { useState, useEffect } from 'react';
 import { x } from '@/assets/icons/svg-icons.js';
+import { useContext } from 'react';
+import { UserContext } from '@/contexts/UserContext';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import ButtonBox from '../ButtonBox';
 import styles from './../project-list-drawer/ProjectListDrawer.module.css';
-import axios from 'axios';
+import { plus } from '@/assets/icons/svg-icons.js';
 
 function ProjectListDrawer({ isOpen, onClose }) {
+  const { userId, projectId, setProjectId } = useContext(UserContext);
+
   const [projectList, setProjectList] = useState([]);
 
   useEffect(() => {
-    const fetchProjectList = async () => {
-      try {
-        const response = await axios.get('http://43.201.85.197/project/', {
-          params: {
-            userId: 2
-          }
-        });
-        const projectNames = response.data.map(project => project.name);
-        setProjectList(projectNames);
-        console.log(projectNames);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
-    fetchProjectList();
-  }, []);
+    axios
+      .get('http://43.201.85.197/project/', {
+        params: {
+          userId: userId,
+        },
+      })
+      .then((response) => {
+        setProjectList(response.data);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
+      });
+  }, [isOpen]);
 
   return (
     <>
@@ -32,17 +35,48 @@ function ProjectListDrawer({ isOpen, onClose }) {
         onClick={onClose}
       />
       <div
-        className={`${styles.drawer} ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`${styles.drawer} ${
+          isOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
       >
         <button className={styles.closeButton} onClick={onClose}>
           <img className="w-full h-full" src={x} alt="닫기" />
         </button>
-        <ul className='flex flex-col gap-4'>
-          {projectList.map((title, index) => (
-            <ButtonBox key={index} mt={0} mb={0} navigateTo='/home'>
-              {title}
+        <ul className="flex flex-col gap-4">
+          {projectList.map((item, index) => {
+            return (
+              <li key={index}>
+                <ButtonBox
+                  status={
+                    item.projectId == Number(projectId) ? 'default' : 'revers'
+                  }
+                  mt={0}
+                  mb={0}
+                  navigateTo="/home"
+                  onClick={() => {
+                    setProjectId(item.projectId);
+                    onClose();
+                  }}
+                >
+                  {item.name}
+                </ButtonBox>
+              </li>
+            );
+          })}
+
+          <li>
+            <ButtonBox
+              status="revers"
+              mt={0}
+              mb={0}
+              navigateTo="/projectname"
+              onClick={() => {
+                onClose();
+              }}
+            >
+              <img src={plus} className="h-6" alt="프로젝트 생성" />
             </ButtonBox>
-          ))}
+          </li>
         </ul>
       </div>
     </>
